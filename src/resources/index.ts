76d67c979c1c184bd1fd647e0a4be304d9d545e1
tungptvn <tungpt.hd@gmail.com>
2017-06-29 10:51:04 +0700

    feat(*): add datepicker control

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -4,5 +4,7 @@ import { FrameworkConfiguration } from 'aurelia-framework';
 export function configure(config: FrameworkConfiguration) {
     config.globalResources([
         PLATFORM.moduleName("./select2/index"),
-        PLATFORM.moduleName("./btn-async/btn-async")]);
+        PLATFORM.moduleName("./btn-async/btn-async"),
+        PLATFORM.moduleName("./datepicker/datepicker"),
+        PLATFORM.moduleName("./datepicker/date-range-picker")]);
 }
